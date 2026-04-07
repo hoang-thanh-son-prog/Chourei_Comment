cde68e2f77fa418dc954241e3c3ff74b986a2bd8
ホアン タインソン <hoang-thanh-son@ad.local>
2026-04-07 13:34:15 +0900

    Create project

---
+++ 朝礼コメント抽選/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace 朝礼コメント抽選;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
